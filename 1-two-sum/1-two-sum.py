5fea0c991a88e170dd555a966a7a957185e764e2
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-08-06 11:24:13 -0700

    Time: 5216 ms (20.17%), Space: 15 MB (77.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,6 @@
-class Solution: #jun 27 22
+class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
+            for j in range(i+1, len(nums)):
                 if target == nums[i] + nums[j]:
-                    return[i, j]
-                
-            #jun 27 22 
+                    return [i, j]
