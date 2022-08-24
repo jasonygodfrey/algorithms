ddee1054eaa39fbaa921f0c5f44d453736c7283f
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-08-23 19:37:36 -0700

    Time: 43 ms (67.42%), Space: 13.9 MB (19.29%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        new_head = head.next
+        head.next = self.swapPairs(head.next.next)
+        new_head.next = head
+        return new_head
