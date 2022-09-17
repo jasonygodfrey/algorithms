3492ac313a1d65f76a7efa22f250c4292d31de29
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 02:05:10 -0400

    Time: 316 ms (22.20%), Space: 16.8 MB (19.10%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        res = 0
         for i in nums:
-            if nums.count(i) == 1:
-                return i
-            
-""" Here is the explanation for the code above:
-1. We create a for loop that will iterate over each element in the list.
-2. Then we check if the element occurs only once in the list by using the count() method.
-3. If the element occurs only once, we return it. """
+            res = i ^ res
+        return res
