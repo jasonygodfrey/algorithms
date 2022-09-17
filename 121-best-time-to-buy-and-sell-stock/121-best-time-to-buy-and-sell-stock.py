80cbfa2d565309dd1ba7f0319e9502458be28910
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 16:44:20 -0400

    Time: 1596 ms (57.06%), Space: 25.1 MB (6.99%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -10,3 +10,10 @@ class Solution:
             else:
                 max_profit = max(max_profit, prices[i] - min_price)
         return max_profit
+    
+    """ Here is the explanation for the code above:
+1. We initialize the maximum profit to be 0, and the minimum price to be the first element in the prices array.
+2. We iterate through the prices array starting from the second element.
+3. If the current element is smaller than the minimum price, we update the minimum price to be the current element.
+4. Otherwise, we update the maximum profit to be the maximum of the current maximum profit and the difference between the current element and the minimum price.
+5. We return the maximum profit at the end of the function. """
