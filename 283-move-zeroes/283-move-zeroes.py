3b8e77a9fa5d3850e1f2c54cad3f89622533858b
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 01:40:34 -0400

    Time: 1098 ms (13.51%), Space: 15.6 MB (16.52%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -6,3 +6,8 @@ class Solution:
                 nums.remove(nums[i])
                 nums.append(0)
         return nums
+    
+""" The code above does the following, explained in English:
+1. Iterate through the list of numbers.
+2. If the number is zero, remove it from the list and append it to the end of the list.
+3. Return the list. """
