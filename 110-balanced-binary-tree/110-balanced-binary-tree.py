6367e1173e2d7d23524d7862d86c00777a27d070
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 02:10:46 -0400

    Time: 150 ms (5.17%), Space: 18.7 MB (60.73%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -13,3 +13,9 @@ class Solution:
         if not root:
             return True
         return abs(height(root.left)-height(root.right))<=1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+    """ Here is the explanation for the code above:
+1. We have a function height which takes the root of the tree as its argument.
+2. Then we check if the root is null, if it is null, we return 0.
+3. If it isn't, we return the height of the left subtree and the right subtree, whichever is greater.
+4. Then we check if the root is null, if it is, we return True.
+5. We return the absolute difference between the height of the left subtree and the right subtree, and we check if it is less than or equal to 1, and we also check if the left subtree is balanced and if the right subtree is balanced. """
