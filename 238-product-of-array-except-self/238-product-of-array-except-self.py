2c241986b5eeed5805dda8d835a0f851a22ea786
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 18:28:22 -0400

    Time: 614 ms (8.57%), Space: 21.4 MB (58.45%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -10,3 +10,9 @@ class Solution:
             answer[i] = answer[i] * R
             R *= nums[i]
         return answer
+""" Here is the explanation for the code above:
+1. First we create the variables we need, namely max_profit to store the maximum profit we can get, and min_price to store the minimum price we have seen so far.
+2. Then we iterate through the prices array, at each index i.
+3. If we have seen a price lower than the current minimum price, we update the minimum price.
+4. Otherwise, we update the maximum profit we can get, by comparing the current maximum profit and the price at index i minus the current minimum price.
+5. Finally, we return the maximum profit we can get. """
