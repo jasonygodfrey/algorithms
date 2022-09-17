aa66e4794f62230cbe05aa497154d5f2cdee18f0
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 02:00:01 -0400

    Time: 7995 ms (5.00%), Space: 16.7 MB (49.43%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -3,3 +3,8 @@ class Solution:
         for i in nums:
             if nums.count(i) == 1:
                 return i
+            
+""" Here is the explanation for the code above:
+1. We create a for loop that will iterate over each element in the list.
+2. Then we check if the element occurs only once in the list by using the count() method.
+3. If the element occurs only once, we return it. """
