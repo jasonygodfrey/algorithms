8509ba15a911ecd1f3dc4d045ce2e6a3c905dc63
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-17 22:24:04 -0400

    Time: 1064 ms (61.56%), Space: 28.5 MB (10.55%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -6,3 +6,11 @@ class Solution:
             current_sum = max(nums[i], current_sum + nums[i])
             max_sum = max(max_sum, current_sum)
         return max_sum
+    """ Here is the explanation for the code above:
+1. Create a variable called max_sum and assign it to the first element of the array.
+2. Create a variable called current_sum and assign it to the first element of the array.
+3. Loop through the array from the second element to the last element.
+4. In each iteration, calculate the maximum between the current element and the sum of the current element and the current_sum.
+5. Update the current_sum with the maximum value.
+6. Calculate the maximum between the max_sum and the current_sum.
+7. Return the max_sum. """
