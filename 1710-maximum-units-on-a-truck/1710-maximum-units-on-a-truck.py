6bce21ae00c93976c319519829c691dd4b80af65
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-18 06:46:51 -0400

    Time: 416 ms (16.76%), Space: 14.4 MB (82.58%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -11,3 +11,12 @@ class Solution:
             truckSize -= numboxes
         
         return total_units
+""" The code above does the following, explained in English:
+1. sort the list of boxes by unit size from largest to smallest. 
+2. iterate through the list, keeping track of how many boxes are left in the truck.
+3. if the number of boxes left in the truck is greater than or equal to the number of boxes in the current box type, 
+   add the number of boxes times its unit size to the total units. 
+4. if the number of boxes left in the truck is less than the number of boxes in the current box type, 
+   add the number of boxes left in the truck times its unit size to the total units. 
+5. if the number of boxes left in the truck is less than 0, break out of the loop. 
+6. return the total units. """
