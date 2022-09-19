c6a4f3f9be19aea4397a09c46a01e1e910557d5c
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-18 20:58:36 -0400

    Time: 99 ms (21.02%), Space: 13.9 MB (30.15%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -19,3 +19,8 @@ class Solution:
                 result += roman[s[i]]
                 i += 1
         return result
+    """ Here is the explanation for the code above:
+1. We can see that the the roman number is composed by some basic symbols, such as I, V, X, L, C, D, M. We can use a dictionary to store the value of each symbol.
+2. We can see that the value of the roman number is composed by some basic symbols, such as I, V, X, L, C, D, M. We can use a dictionary to store the value of each symbol.
+3. We use the while loop to go through the string s. We check if the current symbol is less than the next symbol. If so, we add the value of the next symbol minus the current symbol to the result. We also add 2 to the index of the string. If not, we add the value of the current symbol to the result. We also add 1 to the index of the string.
+4. Finally, we return the result. """
