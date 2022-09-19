cbb43a7b9f17d758cfd422412a5b9cb8c519845e
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-19 07:25:33 -0400

    Time: 1040 ms (5.04%), Space: 21.8 MB (28.99%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -32,3 +32,16 @@ class Solution:
                     bfs(r, c)
                     islands += 1
         return islands
+    
+    """ Here is the explanation for the code above:
+1. First we check if the grid is empty or not. If it is empty we return 0.
+2. Then we find the number of rows and columns in the grid.
+3. We create a set to keep track of the visited cells.
+4. We create a variable to count the number of islands.
+5. We create a function to perform a breadth first search from a given cell.
+6. We start a for loop to iterate over the rows.
+7. We start a for loop to iterate over the columns.
+8. We check if the current cell is a land cell and if it has been visited or not.
+9. If the current cell is a land cell and it has not been visited we perform a breadth first search from the current cell.
+10. We increment the number of islands by 1.
+11. We return the number of islands. """
