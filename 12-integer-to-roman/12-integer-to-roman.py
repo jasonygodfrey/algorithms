ad05a7f5316e26d72922794fd7d042a6d7aad808
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-18 21:19:19 -0400

    Time: 58 ms (84.94%), Space: 13.9 MB (35.47%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,14 +1,11 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50], ["XC", 90], ["C", 100], ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]]
-        
-        res = ""
-        for sym, val in reversed(symList):
-            if num // val:
-                count = num // val
-                res += sym * count
-                num = num % val
-        return res
-    """1. We store the symbols and their values in a list of lists.
-2. We iterate through the list from the end to the beginning, and for each symbol, we divide the given number by the value of the symbol. If the result is greater than zero, it means that the symbol is smaller than the given number. We add the symbol to the result string as many times as the quotient is, and update the number by taking the remainder of the division.
-3. We return the result string. """
+        roman = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+        roman_num = ''
+        while num > 0:
+            for i in sorted(roman.keys(), reverse=True):
+                if num >= i:
+                    roman_num += roman[i]
+                    num -= i
+                    break
+        return roman_num
