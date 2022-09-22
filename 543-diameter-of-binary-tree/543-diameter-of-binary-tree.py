203d6fb33c978b5b4dfa63eefd58daf9567c1e1a
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-22 07:59:45 -0400

    Time: 93 ms (25.99%), Space: 16.5 MB (10.35%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -16,3 +16,13 @@ class Solution:
             return max(left, right) + 1
         dfs(root)
         return res[0]
+
+""" Here is the explanation for the code above:
+1. define a variable res to store the maximum diameter we have found so far, and initialize it to 0
+2. define a function dfs to recursively find the maximum diameter
+3. in the function dfs, we first check if the root is None, if it is, then return -1
+4. if the root is not None, then we recursively call dfs on the left and right child, and assign the result to left and right
+5. we update the maximum diameter we have found so far by comparing the current res with the sum of left and right
+6. we return the maximum of left and right plus 1
+7. we call dfs on the root
+8. we return the result stored in res """
