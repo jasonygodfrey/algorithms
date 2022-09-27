42544370dbc7c5da4612e0db420c77f909408bf8
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-27 12:33:11 -0700

    Time: 616 ms (72.16%), Space: 15.6 MB (24.10%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -15,3 +15,10 @@ class Solution:
                 l = k + 1
                 
         return res
+    """ Here is the explanation for the code above:
+1. we have a range of possible k values: 1 to max(piles) (in the example above, it's 1 to 10)
+2. we use binary search to find the minimum k that satisfies the condition
+3. in each iteration, we calculate the total hours needed to eat all the bananas with k = (l + r) // 2
+4. if the hours is less than h, we update the result and search in the left half of the range
+5. if the hours is greater than h, we search in the right half of the range
+6. the loop terminates when l > r """
