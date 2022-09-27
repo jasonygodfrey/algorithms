57bbdc75feb9e3aa5cce6aeb8566638b6cb18d03
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-26 20:05:17 -0400

    Time: 270 ms (31.46%), Space: 14.9 MB (40.96%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -12,4 +12,11 @@ class Solution:
             else:
                 return [l + 1, r + 1]
         return []
-        
+        """ Here is the explanation for the code above:
+1. Initialize two pointers, left and right.
+2. While left pointer is smaller than right pointer:
+    a. Find the sum of the two numbers pointed by the two pointers.
+    b. If the sum is greater than the target, move the right pointer to the left.
+    c. If the sum is smaller than the target, move the left pointer to the right.
+    d. If the sum is equal to the target, return the result.
+3. If the two pointers meet each other, return the result. """
