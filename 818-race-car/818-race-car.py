8c955d544558ad4c1ebcb03a42c3a2e7f5d26c36
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-09-27 11:41:05 -0400

    Time: 92 ms (56.32%), Space: 16 MB (36.75%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -17,4 +17,17 @@ class Solution:
                     speed = -1 if speed > 0 else 1
                     queue.append((moves + 1, position, speed))
                 
-        
+        """ Here is the explanation for the code above:
+1. The code uses a BFS algorithm to find the optimal path.
+2. The queue is used to store the moves, position, and speed.
+3. The visited set is used to prevent the code from entering an infinite loop.
+4. The first while loop is used to iterate through the queue until it is empty.
+5. The second while loop is used to iterate through the queue until it is empty.
+6. The first if statement checks if the current position is equal to the target. If it is, then the function returns the number of moves.
+7. The second if statement checks if the current position and speed has been visited. If it has, then the code continues to the next iteration.
+8. The else statement adds the current position and speed to the visited set and appends the number of moves, position, and speed * 2 to the queue.
+9. The third if statement checks if the current position and speed is greater than the target and if the speed is positive or if the current position and speed is less than the target and if the speed is negative. If it is, then the speed is changed to negative.
+10. The fourth if statement appends the number of moves, position, and the new speed to the queue.
+11. The function returns -1 if the target is not found.
+12. The time complexity of the code above is O(n^2) where n is the target.
+13. The space complexity of the code above is O(n^2) where n is the target. """
