c0a33faffd72bc6dd07490abb8b4533bf9c821cd
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-10-03 21:18:59 -0700

    Time: 2111 ms (40.22%), Space: 18.1 MB (71.74%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -24,3 +24,14 @@ class Solution:
                 hi -= 1
                 while lo < hi and nums[lo] == nums[lo - 1]:
                     lo += 1
+                    """ Here is the explanation for the code above:
+1. sort the array
+2. use a for loop to loop through the array
+3. if the number is greater than 0, break the loop
+4. if the number is not the same as the number before it, call the function twoSumII
+5. in the function twoSumII, use two pointers lo and hi to find two numbers that add up to the negative of the number at i
+6. if the sum is less than 0, move the lo pointer to the right
+7. if the sum is greater than 0, move the hi pointer to the left
+8. if the sum is 0, add the three numbers to the result list and move the lo and hi pointers to the right and left
+9. if the lo pointer is the same as the number before it, move the lo pointer to the right
+10. if the hi pointer is the same as the number after it, move the hi pointer to the left """
