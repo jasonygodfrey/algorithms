1880d5dbf9ae55afe8fc910bba34ac41d5bbe126
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-10-15 02:14:46 -0700

    Time: 76 ms (30.78%), Space: 13.8 MB (98.75%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -24,3 +24,11 @@ class Solution:
             
         
         return dummy.next
+    """ Here is the explanation for the code above:
+1. The dummy node is used to simplify some corner cases such as a list with only one node, or empty list. On the other hand, the dummy is used to build up the result list.
+2. We need two pointers, l1 and l2, to traverse the lists l1 and l2.
+3. The tail pointer is used to build up the result list.
+4. We compare the two nodes pointed by l1 and l2, and append the node with a smaller value to the result list.
+5. We then advance the pointer of the list that we just appended.
+6. The while loop terminates when either l1 or l2 points to None.
+7. We then append the non-None list to the result list and return the result. """
