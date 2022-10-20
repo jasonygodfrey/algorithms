bec3a974a1642574b62e0df3a1792a77ff3ed492
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-10-20 13:39:20 -0700

    Time: 37 ms (98.70%), Space: 16.4 MB (23.51%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -10,3 +10,11 @@ class Solution:
             return 0
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+
+        #time complexity O(n) 
+        # space complexity O(n) 
+""" Here is the explanation for the code above:
+1. The base case: if the root is None, return 0, because the depth is 0.
+2. Recursion: return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+   This is the recursion step. The recursion step is to find the maximum depth of the left subtree and the right subtree. And the maximum depth of the root is 1 + max(left subtree's depth, right subtree's depth). """
