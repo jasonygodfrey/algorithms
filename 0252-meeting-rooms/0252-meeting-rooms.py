b395adac522be3d2100ee41f68c8b6465786ca5f
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-10-21 11:23:28 -0700

    Time: 169 ms (36.25%), Space: 17.3 MB (78.39%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -5,3 +5,13 @@ class Solution:
             if intervals[i][1] > intervals[i+1][0]:
                 return False
         return True
+        
+
+
+        #time complexity O(nlogn)
+        # space complexity O(1)
+""" Here is the explanation for the code above:
+1. We sort the list of intervals by the start time of each interval.
+2. We iterate through the list of intervals and check if each end time is less than or equal to the start time of the next interval.
+3. If the end time is greater than the start time of the next interval, we return False.
+4. If we reach the end of the list, we return True. """
