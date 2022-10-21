baac95522063380693320ffbe9bc20fc338978d0
Jason Godfrey <83245646+jasonygodfrey@users.noreply.github.com>
2022-10-21 11:50:56 -0700

    Time: 103 ms (94.72%), Space: 19.8 MB (27.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -9,3 +9,9 @@ class Solution:
             res[tuple(count)].append(s)
             
         return res.values()
+    """ Here is the explanation for the code above:
+1. defaultdict(list) creates a dict with list as default value. 
+2. count = [0] * 26 is a list of 26 zeros. 
+3. count[ord(c) - ord("a")] += 1 means that the count of the letter c is increased by 1. 
+4. res[tuple(count)].append(s) means that we map the count to the anagram. 
+5. res.values() returns the values of the dict which is a list of lists. """
